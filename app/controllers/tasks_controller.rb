77b0338b1229c981c2a0ca121e44624d504dc96b
Devin Dow <DevinDow@gmail.com>
2018-09-08 06:01:59 -0700

    on task update redirect_to list_path(@task.id) (because I removed tasks.show)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to list_path(@task.list_id), notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
