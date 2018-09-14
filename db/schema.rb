6c83e068242808a56999e108d49a22d6566905d3
Devin Dow <DevinDow@gmail.com>
2018-09-14 09:20:26 -0700

    "rake db:migrate"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_125143) do
+ActiveRecord::Schema.define(version: 2018_09_14_161639) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2018_09_06_125143) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_tasks_on_list_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
