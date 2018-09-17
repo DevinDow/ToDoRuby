ae6121fed4602d4ba35721de3185c5700e259a0b
Devin Dow <DevinDow@gmail.com>
2018-09-17 16:06:54 -0700

    "rails generate integration_test users_signup", improved routes

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.3.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
+gem 'rails-controller-testing'
 # Bootstrap
 gem 'bootstrap-sass'
 # bcrypt for hashed password
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+
+  # Home
   root to: 'home#index'
 
+  # Users
   get 'users/new'
+  get 'signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
+  # Lists & Tasks
   get 'tasks', to: 'tasks#index', as: 'tasks'
 
   resources :lists do
     resources :tasks, shallow: true, :except => [:index, :show]
   end
-
-  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
