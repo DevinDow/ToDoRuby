f22ec8b70f4451a8d60c4352aef90fe523c09905
Devin Dow <DevinDow@gmail.com>
2018-09-18 15:11:14 -0700

    log_in @user on sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html do 
+          log_in @user
+          redirect_to @user, notice: 'User was successfully created.'
+        end
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
