05c4dfd9b3258e5df7fe7b1fe5d379cc18f2d728
Devin Dow <DevinDow@gmail.com>
2018-09-19 11:42:50 -0700

    login redirects to root

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -22,9 +22,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email, password: "password" } }
     assert is_logged_in?
-    assert_redirected_to @user
+    assert_redirected_to root_path
     follow_redirect!
-    assert_template 'users/show'
     # check for appropriate menus
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
