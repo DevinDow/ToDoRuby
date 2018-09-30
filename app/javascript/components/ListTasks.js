782974a3b8dacfcaf3e2f7dc985c1c287fa2aae7
Devin Dow <DevinDow@gmail.com>
2018-09-30 08:45:44 -0700

    ListTasks React Component fetches Tasks

--- app/javascript/components/ListTasks.js
+++ app/javascript/components/ListTasks.js
@@ -6,16 +6,32 @@ class ListTasks extends React.Component {
     super(props);
     console.log("ListTasks.constructor() props.list=" + props.list);
     this.state = {
-      list: props.list
+      list: props.list,
+      tasks: []
     };
+  }
+
+  componentDidMount(){
+    console.log("ListTasks.componentDidMount()");
+    fetch(Routes.list_tasks_path(this.props.list.id) + '.json')
+      .then((response) => {return response.json()})
+      .then((data) => {this.setState({ tasks: data }) });
   }
 
   render () {
     console.log("ListTasks.render() list.name=" + this.state.list.name);
     console.log("ListTasks.render() list.tasks=" + this.state.list.tasks);
+    console.log("ListTasks.render() tasks=" + this.state.tasks);
     return (
       <React.Fragment>
         <h4>ListTasks React Component for {this.state.list.name}</h4>
+        {
+          this.state.tasks.map((task) => {
+            return(
+              <h5>{task.name}</h5>
+            )
+          })
+        }
       </React.Fragment>
     );
   }
