016ec00184312b336540c3fe97e6b49a14d913b1
Devin Dow <DevinDow@gmail.com>
2018-10-01 20:51:26 -0700

    Main React Component converted to stateless

--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -1,22 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 import MyLists from "./MyLists";
-class Main extends React.Component {
+const Main = (props) => {
 
-  constructor(props) {
-    super(props);
-    console.log("Main.constructor()");
-  }
+  console.log("Main(" + props + ")");
 
-  render () {
-    console.log("Main.render()");
-    return (
-      <React.Fragment>
-        <MyLists my_lists={this.props.my_lists} />
-        <a href='/lists/new'>New List</a>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <MyLists my_lists={props.my_lists} />
+      <a href='/lists/new'>New List</a>
+    </React.Fragment>
+  );
 }
 
 export default Main
