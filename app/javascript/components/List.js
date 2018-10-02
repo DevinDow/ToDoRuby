61481a54e76acc50aac4556b36b2bfca65d266ef
Devin Dow <DevinDow@gmail.com>
2018-10-01 23:04:43 -0700

    "rails g react:component NewTask", WIP: flushing out controls

--- app/javascript/components/List.js
+++ app/javascript/components/List.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import ListTasks from "./ListTasks";
+import NewTask from "./NewTask";
 const List = (props) => {
 
   console.log("List " + props.list.id + " = " + props.list.name);
@@ -10,6 +11,7 @@ const List = (props) => {
     <div>
       <h2><a href={`/lists/${props.list.id}`}>{props.list.name}</a></h2>
       <ListTasks list={props.list} />
+      <NewTask list_id={props.list.id} />
       <a href={`/lists/${props.list.id}/tasks/new`}>New Task</a>
     </div>
   );
---
+++ app/javascript/components/NewTask.js
@@ -0,0 +1,48 @@
+import React from "react"
+import PropTypes from "prop-types"
+class NewTask extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log("NewTask");
+    console.log(props)
+    this.state = {
+      creating: false
+    }
+    this.toggleCreating = this.toggleCreating.bind(this)
+    this.handleCreate = this.handleCreate.bind(this)
+  }
+
+  toggleCreating() {
+    this.setState({creating: !this.state.creating})
+  }
+
+  handleCreate() {
+    if (this.state.creating) {
+      this.props.handleUpdate({
+        priority: this.priority.value, 
+        name: this.name.value
+      })
+    }
+
+    // toggle creating
+    this.setState({creating: !this.state.creating})
+  }
+
+  render () {
+    let priority = this.state.creating ? <input className="priority" type="number" ref={input => this.priority = input} /> : "";
+    let name = this.state.creating ? <input className="name" type="text" ref={input => this.name = input} /> : "";
+    let createButton = this.state.creating ? <button onClick={() => this.handleCreate()}>Create</button> : "";
+    return (
+      <div className="task">
+        {priority}
+        {name}
+        {createButton}
+        <button onClick={() => this.toggleCreating()}>{this.state.creating ? 'Cancel' : 'New Task'}</button>
+      </div>
+    );
+  }
+
+}
+
+export default NewTask
