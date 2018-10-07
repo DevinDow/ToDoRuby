2578271e6852052a051447a2dd6ca15aac29d920
Devin Dow <DevinDow@gmail.com>
2018-10-07 11:56:06 -0700

    v-if for showing <span> or <input>.  TODO: Submit, Delete

--- app/javascript/Task.vue
+++ app/javascript/Task.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class="task {this.props.task.done ? 'completed' : ''}">
-    <input class="done" type="checkbox" />
+  <div v-if="editable" class="task {this.props.task.done ? 'completed' : ''}">
+    <input class="done" type="checkbox" v-model="this.$props.task.done" />
+    <input class="priority" v-model="this.$props.task.priority" />
+    <input class="name" v-model="this.$props.task.name" />
+    <button>Submit</button>
+    <button v-on:click="editable=false">Cancel</button>
+    <button>Delete</button>
+  </div>
+  <div v-else class="task {this.props.task.done ? 'completed' : ''}">
+    <input class="done" type="checkbox" v-model="this.$props.task.done" />
     <span class="priority">{{this.$props.task.priority}}</span>
     <span class="name">{{this.$props.task.name}}</span>
-    <button>Edit</button>
+    <button v-on:click="editable=true">Edit</button>
     <button>Delete</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["task"],
+  props: [
+    "task"
+  ],
 
-  mounted: function () {
-    console.log("**Task MOUNTED**")
-    console.log(this)
+  data: function () {
+    return {
+      editable: false
+    }
   }
 }
 </script>
