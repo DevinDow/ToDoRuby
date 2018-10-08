2424e86ad9098ae876ba2fc52a838d9d24ad9d30
Devin Dow <DevinDow@gmail.com>
2018-10-07 23:50:21 -0700

    "editable" renamed to "editing"

--- app/javascript/react_components/Task.js
+++ app/javascript/react_components/Task.js
@@ -6,7 +6,7 @@ class Task extends React.Component {
     console.log("Task " + props.task.id + " = " + props.task.name);
     console.log(props)
     this.state = {
-      editable: false
+      editing: false
     }
     this.handleDone = this.handleDone.bind(this)
     this.toggleEdit = this.toggleEdit.bind(this)
@@ -14,12 +14,12 @@ class Task extends React.Component {
   }
 
   toggleEdit() {
-    this.setState({editable: !this.state.editable});
+    this.setState({editing: !this.state.editing});
   }
 
   handleDone() {
-    // allow checking Done when not editable to do an update
-    if (!this.state.editable) {
+    // allow checking Done when not editing to do an update
+    if (!this.state.editing) {
       this.props.handleUpdate({
         id: this.props.task.id,
         done: this.done.checked, 
@@ -30,7 +30,7 @@ class Task extends React.Component {
   }
 
   handleSubmit() {
-    if (this.state.editable) {
+    if (this.state.editing) {
       this.props.handleUpdate({
         id: this.props.task.id,
         done: this.done.checked, 
@@ -42,16 +42,16 @@ class Task extends React.Component {
   }
 
   render () {
-    let priority = this.state.editable ? <input className="priority" type="number" ref={input => this.priority = input} defaultValue={this.props.task.priority} /> : <span className="priority">{this.props.task.priority}</span>;
-    let name = this.state.editable ? <input className="name" type="text" ref={input => this.name = input} defaultValue={this.props.task.name} /> : <span className="name">{this.props.task.name}</span>;
-    let submit = this.state.editable ? <button onClick={() => this.handleSubmit()}>Submit</button> : "";
+    let priority = this.state.editing ? <input className="priority" type="number" ref={input => this.priority = input} defaultValue={this.props.task.priority} /> : <span className="priority">{this.props.task.priority}</span>;
+    let name = this.state.editing ? <input className="name" type="text" ref={input => this.name = input} defaultValue={this.props.task.name} /> : <span className="name">{this.props.task.name}</span>;
+    let submit = this.state.editing ? <button onClick={() => this.handleSubmit()}>Submit</button> : "";
     return (
       <div className={this.props.task.done ? "task completed" : "task"}>
         <input className="done" type="checkbox" ref={input => this.done = input} defaultChecked={this.props.task.done} onClick={() => this.handleDone()} />
         {priority}
         {name}
         {submit}
-        <button onClick={() => this.toggleEdit()}>{this.state.editable? 'Cancel' : 'Edit'}</button>
+        <button onClick={() => this.toggleEdit()}>{this.state.editing ? 'Cancel' : 'Edit'}</button>
         <button onClick={() => this.props.handleDelete(this.props.task.id)} data-confirm="Are you sure?">Delete</button>
       </div>
     );
--- app/javascript/vue_components/Task.vue
+++ app/javascript/vue_components/Task.vue
@@ -1,17 +1,17 @@
 <template>
-  <div v-if="editable" class="task {this.props.task.done ? 'completed' : ''}">
+  <div v-if="editing" class="task {this.props.task.done ? 'completed' : ''}">
     <input class="done" type="checkbox" v-model="task.done" />
     <input class="priority" type="number" v-model="task.priority" />
     <input class="name" v-model="task.name" />
-    <button v-on:click="$emit('submit', task); editable=false">Submit</button>
-    <button v-on:click="editable=false">Cancel</button>
+    <button v-on:click="$emit('submit', task); editing=false">Submit</button>
+    <button v-on:click="editing=false">Cancel</button>
     <button>Delete</button>
   </div>
   <div v-else class="task {this.props.task.done ? 'completed' : ''}">
     <input class="done" type="checkbox" v-model="task.done" v-on:click="$emit('submit', task)" />
     <span class="priority">{{this.$props.task.priority}}</span>
     <span class="name">{{this.$props.task.name}}</span>
-    <button v-on:click="editable=true">Edit</button>
+    <button v-on:click="editing=true">Edit</button>
     <button>Delete</button>
   </div>
 </template>
@@ -24,13 +24,13 @@ export default {
 
   data: function () {
     return {
-      editable: false
+      editing: false
     }
   },
 
   methods: {
     toggleEdit() {
-      this.editable = !this.editable;
+      this.editing = !this.editing;
     },
   }
 }
