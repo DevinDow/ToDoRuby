df2062d06cfff3631de16e62936a9ab5266db3f4
Devin Dow <DevinDow@gmail.com>
2018-10-09 09:15:16 -0700

    NewTask focuses "name" when startCreating()

--- app/javascript/vue_components/NewTask.vue
+++ app/javascript/vue_components/NewTask.vue
@@ -2,12 +2,12 @@
   <form v-if="creating" class="task" @submit.prevent="$emit('create', task); cancelCreating()" @keydown.esc="cancelCreating">
     <input class="done" type="checkbox" v-model="task.done" />
     <input class="priority" type="number" v-model="task.priority" />
-    <input class="name" v-model="task.name" />
+    <input class="name" ref="name" v-model="task.name" autofocus />
     <button type="submit">Create</button>
     <button type="button" @click="cancelCreating">Cancel</button>
   </form>
   <div v-else class="task">
-    <button @click="creating=true; ">New Task</button>
+    <button @click="startCreating">New Task</button>
   </div>
 </template>
 
@@ -21,6 +21,11 @@ export default {
   },
 
   methods: {
+    startCreating() {
+      this.creating=true
+      this.$nextTick(() => this.$refs.name.focus())
+    },
+
     cancelCreating() {
       this.creating = false
       this.task = {priority: 1}
