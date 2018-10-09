eeb5149cd3b9b9bb63982cb0b15e828aa7ed5aaa
Devin Dow <DevinDow@gmail.com>
2018-10-09 09:37:20 -0700

    startCreating() inits task, so cancelCreating not needed

--- app/javascript/vue_components/NewTask.vue
+++ app/javascript/vue_components/NewTask.vue
@@ -1,10 +1,10 @@
 <template>
-  <form v-if="creating" class="task" @submit.prevent="$emit('create', task); cancelCreating()" @keydown.esc="cancelCreating">
+  <form v-if="creating" class="task" @submit.prevent="$emit('create', task); creating=false" @keydown.esc="creating=false">
     <input class="done" type="checkbox" v-model="task.done" />
     <input class="priority" type="number" v-model="task.priority" />
     <input class="name" ref="name" v-model="task.name" autofocus />
     <button type="submit">Create</button>
-    <button type="button" @click="cancelCreating">Cancel</button>
+    <button type="button" @click="creating=false">Cancel</button>
   </form>
   <div v-else class="task">
     <button @click="startCreating">New Task</button>
@@ -16,19 +16,15 @@ export default {
   data: function () {
     return {
       creating: false,
-      task: {priority: 1}
+      task: {}
     }
   },
 
   methods: {
     startCreating() {
       this.creating=true
-      this.$nextTick(() => this.$refs.name.focus())
-    },
-
-    cancelCreating() {
-      this.creating = false
       this.task = {priority: 1}
+      this.$nextTick(() => this.$refs.name.focus())
     }
   }
 }
