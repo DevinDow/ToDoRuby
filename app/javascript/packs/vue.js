a7624a91761013d9d6900919e4b28bb98d0188fa
Devin Dow <DevinDow@gmail.com>
2018-10-11 12:03:56 -0700

    Vue.config.devtools = true

--- app/javascript/packs/vue.js
+++ app/javascript/packs/vue.js
@@ -1,5 +1,7 @@
 import Vue from 'vue/dist/vue.esm'
 console.log("*** Vue.config.devtools = " + Vue.config.devtools);
+Vue.config.devtools = true
+console.log("*** Vue.config.devtools = " + Vue.config.devtools);
 
 import TurbolinksAdapter from 'vue-turbolinks'
 Vue.use(TurbolinksAdapter)
