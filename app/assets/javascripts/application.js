e637405deedf760c319a1dc578ed9eea2aa161a1
Devin Dow <DevinDow@gmail.com>
2018-10-11 12:19:11 -0700

    experimenting with removing "require's

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,5 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
 //= require turbolinks
 //= require_tree .
