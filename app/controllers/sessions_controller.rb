b2c470888243ee3bd7d131776d1c8a59f3408128
Devin Dow <DevinDow@gmail.com>
2018-10-18 08:32:14 -0700

    handle POST /login.json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,19 @@ class SessionsController < ApplicationController
   # POST /login
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
-      log_in user
-      redirect_to root_url
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+    respond_to do |format|
+      if user && user.authenticate(params[:session][:password])
+        # Log the user in and redirect to the user's show page.
+        log_in user
+        format.html { redirect_to root_url }
+        format.json { render json: {user: cookies[:todo_user_id]}, status: :created }
+      else
+        format.html { 
+          flash.now[:danger] = 'Invalid email/password combination'
+          render 'new'
+         }
+        format.json { render json: {message: 'failed'}, status: :bad }
+      end
     end
   end
 
